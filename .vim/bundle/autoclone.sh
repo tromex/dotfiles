c42e600678c8bb34aba6dcd2529fa388d5c3f0ae
tromex <tromersebastian@gmail.com>
2016-09-29 20:23:33 +0200

    auto clone bundle plugins for vim

---
+++ .vim/bundle/autoclone.sh
@@ -0,0 +1,4 @@
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
