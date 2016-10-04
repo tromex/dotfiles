87491711c47c4c1bd9f20821e46cb7eaf9c32c26
Sebastian Tromer <s.tromer@cs-group.de>
2016-10-04 13:46:34 +0200

    Added wombat256mod to vim bundle

--- .vim/bundle/autoclone.sh
+++ .vim/bundle/autoclone.sh
@@ -3,3 +3,4 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/ctrlp.vim
 git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
+git clone https://github.com/michalbachowski/vim-wombat256mod
